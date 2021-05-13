235f4d4e49d4dbc7a1fcff5f1b98659aeec0cfa6
minajalili <minajalili1997@gmail.com>
2021-05-13 10:30:54 +0430

    add CalenderBar component to project

--- src/components/CalenderBar.js
+++ src/components/CalenderBar.js
@@ -6,7 +6,37 @@ function CalenderBar(){
 
     return(
         <div className="CalenderBar">
-            <h1>CalenderBar</h1>
+            <div className="Calender--detail">
+                <div className="Current--Month">
+                    <h1>November 2019</h1>
+                </div>
+                <div className="Current--date">
+                    <h5>Today</h5>
+                    <p>18, Mon</p>
+                </div>
+                <div className="Search--Item">
+                    <button>
+                        <i className="fas fa-search" ></i>
+                        <span>Search</span>
+                    </button>
+                </div>
+            </div>
+            <div className="Calender--mode">
+                <i className="fas fa-chevron-left" ></i>
+                <button className="Calender--mode--day" >Day</button>
+                <button className="Calender--mode--week" >Week</button>
+                <button className="Calender--mode--month" >Month</button>
+                <i className="fas fa-chevron-right" ></i>
+            </div>
+            <div className="Calender--days">
+                <p>sun</p>
+                <p>Mon</p>
+                <p>Tue</p>
+                <p>Wen</p>
+                <p>Thu</p>
+                <p>Fri</p>
+                <p>Sat</p>
+            </div>
         </div>
     )
 }
